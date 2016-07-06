564c61f982ebf9440cbbc2a43c718482a6bcf974
Evan Danaher <edanaher@edanaher.net>
2016-07-06 14:56:51 -0400

    Take snapshots every hour.  They're fast with --delete...

--- snapshot.nix
+++ snapshot.nix
@@ -3,7 +3,7 @@
 {
   services.fcron.enable = true;
   services.fcron.systab = ''
-    0 7 * * * /mnt/snapshots/do.sh
+    0 * * * * /mnt/snapshots/do.sh
     37 1 * * * { umount /mnt/snapshots && mount /mnt/snapshots; } >/dev/null 2>&1
   '';
 }
