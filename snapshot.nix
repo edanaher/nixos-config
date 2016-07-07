4f7e7f691a66dc27ed758fbb40227df41777b2fa
Evan Danaher <edanaher@edanaher.net>
2016-07-07 07:58:37 -0400

    Don't remount /mnt/snapshots daily.
    Apparently btrfs now frees RAM properly with a modern (>3.16) kernel, so this
    isn't necessary.

--- snapshot.nix
+++ snapshot.nix
@@ -4,6 +4,6 @@
   services.fcron.enable = true;
   services.fcron.systab = ''
     0 * * * * /mnt/snapshots/do.sh
-    37 1 * * * { umount /mnt/snapshots && mount /mnt/snapshots; } >/dev/null 2>&1
+    #37 1 * * * { umount /mnt/snapshots && mount /mnt/snapshots; } >/dev/null 2>&1
   '';
 }
