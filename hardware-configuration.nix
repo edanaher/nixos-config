3dd664b1f31356a0cd5faa0366269a08b8d90548
Evan Danaher <evan.danaher@logicblox.com>
2016-11-30 00:11:08 +0000

    Update hardware-configuration.nix for gemedet.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,79 +8,35 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_hcd" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "ahci" "rtsx_pci_sdmmc" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8e74b8e7-fd92-4e62-b9b5-55782f48e064";
+    { device = "/dev/disk/by-uuid/1a51ddb0-1625-4db4-8cef-fd79894af395";
       fsType = "btrfs";
       options = [ "subvol=root" "compress" "noatime" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8a67afc2-ddc9-45c9-ad78-64fcf9a48898";
+    { device = "/dev/disk/by-uuid/e53e014b-8fe5-4eb9-be4d-9c3a3f8c284a";
       fsType = "ext2";
     };
 
+  fileSystems."/boot/efi" =
+    { device = "/dev/disk/by-uuid/F49A-AC67";
+      fsType = "vfat";
+    };
+
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/8e74b8e7-fd92-4e62-b9b5-55782f48e064";
+    { device = "/dev/disk/by-uuid/1a51ddb0-1625-4db4-8cef-fd79894af395";
       fsType = "btrfs";
       options = [ "subvol=home" "compress" "noatime" ];
     };
 
-  fileSystems."/home/edanaher/extra" =
-    { device = "/dev/mapper/vgtor-home--extra";
-      fsType = "ext4";
-      options = [ "noatime" "noexec" ];
-    };
-
-  fileSystems."/mnt/snapshots" =
-    { device = "/dev/mapper/vgfah-home--snapshots";
-      fsType = "btrfs";
-      options = [ "compress" "noatime" "nofail" ];
-    };
-
-  fileSystems."/mnt/movies" =
-    { device = "/dev/mapper/vgtor-movies";
-      fsType = "ext4";
-      options = [ "nofail" "noatime" ];
-    };
-
-  fileSystems."/mnt/old" =
-    { device = "/dev/mapper/vgtor-old";
-      fsType = "ext4";
-      options = [ "nofail" "noatime" ];
-    };
-
-  fileSystems."/mnt/bak" =
-    { device = "/dev/mapper/vgtor-bak";
-      fsType = "ext4";
-      options = [ "nofail" "noatime" ];
-    };
-
-  fileSystems."/mnt/external" =
-    { device = "/dev/disk/by-uuid/6d5f4267-2d60-4cd6-b623-3b96793b3529";
-      fsType = "btrfs";
-      options = [ "noauto" "noatime" "nofail" ];
-    };
-
-  fileSystems."/mnt/data" =
-    { device = "/dev/mapper/vgtor-data";
-      fsType = "ext4";
-      options = [ "noatime" "nofail" ];
-    };
-
-  fileSystems."/mnt/kobo" =
-    { device = "/dev/disk/by-uuid/51A9-A1CD";
-      fsType = "vfat";
-      options = [ "noatime" "nofail" "noauto" "user" ];
-    };
-
-
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/7e7b4a0c-3c6e-47e9-9278-ca315d8b83ea"; }
+    [ { device = "/dev/disk/by-uuid/3b19a8af-ddc2-45ce-8545-f47d29e13e26"; }
     ];
 
-  nix.maxJobs = lib.mkDefault 4;
+  nix.maxJobs = lib.mkDefault 8;
 }
