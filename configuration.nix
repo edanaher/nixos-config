a3442941d58c731ed642d028b4a899ef6b30a6e1
Evan Danaher <github@edanaher.net>
2016-12-09 18:23:18 -0500

    Add (non-functional?) Synaptics touchpad configuration.

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,28 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
+  services.xserver.synaptics.enable = true;
+
+  services.xserver.synaptics.accelFactor = "0.05";
+  services.xserver.synaptics.minSpeed = "1";
+  services.xserver.synaptics.maxSpeed = "2.5";
+  services.xserver.synaptics.twoFingerScroll = true;
+  services.xserver.synaptics.vertEdgeScroll = true;
+  services.xserver.synaptics.additionalOptions = ''
+    Option "VertScrollDelta" "16"
+    Option "RTCornerButton" "3"
+    Option "TapButton2" "3"
+    Option "TapButton3" "2"
+    Option "ClickFinger2" "3"
+    Option "ClickFinger3" "2"
+    Option "CircularScrolling" "1"
+    Option "CircScrollTrigger" "2"
+    Option "RightButtonAreaLeft" "560"
+    Option "RightButtonAreaTop" "400"
+    Option "MiddleButtonAreaLeft" "460"
+    Option "MiddleButtonAreaRight" "559"
+    Option "MiddleButtonAreaTop" "400"
+  '';
 
   programs.bash.enableCompletion = true;
 
