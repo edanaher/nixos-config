04f5cf423fdca6db80753eb780e8130b38ffa6ba
Evan Danaher <github@edanaher.net>
2017-02-06 19:55:21 -0500

    Remove hardware-configuration.nix and use hardware-configuration/.

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@ in
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware-configuration
       ./host
       ./hostname.nix
       ./exim.nix
