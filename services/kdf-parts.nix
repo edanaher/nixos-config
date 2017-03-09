7da0ac54a6aba891098adfbdd592673f2c3ccb4d
Evan Danaher <github@edanaher.net>
2017-03-09 20:16:49 +0000

    Add kdf-learn service.  No more ad-hoc services on kdf!

--- services/kdf-parts.nix
+++ services/kdf-parts.nix
@@ -3,6 +3,7 @@
 
 let kdf-web = import /home/edanaher/kdf/web { inherit pkgs; } ;
     kdf-dns = import /home/edanaher/kdf/dns { inherit pkgs; } ;
+    kdf-learn = import /home/edanaher/kdf/learn { inherit pkgs; } ;
 in
 {
   config = lib.mkIf (config.host.name == "kdfsh") {
@@ -24,6 +25,18 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = "${kdf-dns}/bin/kdf-dns";
+        Restart = "always";
+        RestartSec = "10s";
+        StartLimitInterval = "1min";
+      };
+    };
+
+    systemd.services.kdf-learn = {
+      description = "kdf learn web service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${kdf-learn}/bin/learn.exe";
         Restart = "always";
         RestartSec = "10s";
         StartLimitInterval = "1min";
