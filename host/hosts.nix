5b3a98a5c11e12652b8657ff8b4e078bbc1b51bc
Evan Danaher <github@edanaher.net>
2017-03-30 09:51:58 -0400

    Rename snapshots.nix -> chileh-backups.nix.

--- host/hosts.nix
+++ host/hosts.nix
@@ -115,7 +115,7 @@ let
   hostconfig = utils.select config.host.name hosts;
   classconfig = utils.select config.host.class classes;
   # TODO: directly using hostname.nix because of imports and infinite recursion and mkIf fail.
-  hostimports = if (import ../hostname.nix).host.name == "chileh" then [ ../snapshot.nix ] else [];
+  hostimports = if (import ../hostname.nix).host.name == "chileh" then [ ../chileh-backups.nix ] else [];
 in
 {
   imports = hostimports;
