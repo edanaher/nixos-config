cd13311b1fe793a90b84366767a4c31cd9e60dde
Evan Danaher <github@edanaher.net>
2017-04-04 11:00:15 -0400

    exim: Use mkMerge instead of // to avoid breaking clients.

--- exim/default.nix
+++ exim/default.nix
@@ -2,11 +2,13 @@
 
 let secrets = import ../secrets.nix;
     smtp-to-xmpp = import /home/edanaher/smtp-to-xmpp { inherit pkgs; };
+    client-config = lib.mkIf (config.host.class != "server") {
+      services.exim.config = import ./client-conf.nix { inherit secrets; };
+    };
+    server-config = lib.mkIf (config.host.name == "kdfsh") {
+      services.exim.config = import ./kdfsh-conf.nix { inherit smtp-to-xmpp; };
+    };
 in
 {
-  config = lib.mkIf (config.host.class != "server") {
-    services.exim.config = import ./client-conf.nix { inherit secrets; };
-  } // lib.mkIf (config.host.name == "kdfsh") {
-    services.exim.config = import ./kdfsh-conf.nix { inherit smtp-to-xmpp; };
-  };
+  config = lib.mkMerge [ client-config server-config ];
 }
