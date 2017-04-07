d034f0f0264cbc76ff8e78311b46ca1001b37493
Evan Danaher <github@edanaher.net>
2017-04-07 13:52:11 +0000

    ACME cert: Set user to prosody to it can read it.
    These really should be separate certs, but that's not easy yet...

--- services/dovecot.nix
+++ services/dovecot.nix
@@ -10,6 +10,7 @@
       sslServerCert = "/var/lib/acme/kdf.sh/fullchain.pem";
       mailLocation = "maildir:~/.maildir:LAYOUT=fs";
     };
+    security.acme.certs."kdf.sh".user = "prosody";
     security.acme.certs."kdf.sh".group = "exim";
     security.acme.certs."kdf.sh".allowKeysForGroup = true;
   };
