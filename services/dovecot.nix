739272015bb89e0c26677ca24b6c7c946734dbb9
Evan Danaher <github@edanaher.net>
2017-04-08 15:48:16 +0000

    Dovecot: Commented-out using dovecot-specific SSL certificate.
    Unfortunately, I hit the rate limit for letsencrypt certificates on
    kdf.sh for the week.  I'll enable this next week.

--- services/dovecot.nix
+++ services/dovecot.nix
@@ -6,12 +6,19 @@
       enable = true;
       enableImap = true;
       # Certs are generated in kdf-nginx.nix under kdf.sh hostname.
+      # TODO: once the kdf.sh ratelimit on letsencrypt times out, update this.
       sslServerKey = "/var/lib/acme/kdf.sh/key.pem";
       sslServerCert = "/var/lib/acme/kdf.sh/fullchain.pem";
       mailLocation = "maildir:~/.maildir:LAYOUT=fs";
     };
-    security.acme.certs."kdf.sh".user = "prosody";
-    security.acme.certs."kdf.sh".group = "exim";
-    security.acme.certs."kdf.sh".allowKeysForGroup = true;
+    /*security.acme.certs."dovecot-kdf.sh" ={
+      domain = "kdf.sh";
+      user = "root";
+      group = "dovecot2";
+      allowKeysForGroup = true;
+      webroot = config.security.acme.certs."kdf.sh".webroot;
+      postRun = "systemctl reload dovecot2";
+      email = "dovecotcert@kdf.sh";
+    };*/
   };
 }
