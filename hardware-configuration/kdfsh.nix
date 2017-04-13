0b92eabba2d9044f59c30d832b53b826d6404cf8
Evan Danaher <github@edanaher.net>
2017-04-13 13:58:47 +0000

    kdfsh: Set up grub for configfile chaining from Debian grub.

--- hardware-configuration/kdfsh.nix
+++ hardware-configuration/kdfsh.nix
@@ -8,28 +8,34 @@
     "${modulesPath}/virtualisation/xen-domU.nix"
   ];
 
-  boot.initrd.availableKernelModules = [ "xen_blkfront" ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+  config = lib.mkMerge [ {
+    boot.initrd.availableKernelModules = [ "xen_blkfront" ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    boot.loader.grub.extraPerEntryConfig = "root (hd0,4)";
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/9daaaa08-ac9c-42ee-bee2-c7807ffbabfc";
-      fsType = "btrfs";
-      options = [ "subvol=root" "noatime" "compress" ];
-    };
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/9daaaa08-ac9c-42ee-bee2-c7807ffbabfc";
+        fsType = "btrfs";
+        options = [ "subvol=root" "noatime" "compress" ];
+      };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/9daaaa08-ac9c-42ee-bee2-c7807ffbabfc";
-      fsType = "btrfs";
-      options = [ "subvol=home" "noatime" "compress" ];
-    };
+    fileSystems."/home" =
+      { device = "/dev/disk/by-uuid/9daaaa08-ac9c-42ee-bee2-c7807ffbabfc";
+        fsType = "btrfs";
+        options = [ "subvol=home" "noatime" "compress" ];
+      };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/ad834324-780f-4b26-9db2-bbc5a1801eb0";
-      fsType = "ext2";
-    };
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/ad834324-780f-4b26-9db2-bbc5a1801eb0";
+        fsType = "ext2";
+      };
 
-  swapDevices = [ ];
+    swapDevices = [ ];
 
-  nix.maxJobs = lib.mkDefault 1;
+    nix.maxJobs = lib.mkDefault 1;
+  }
+  # xen-domU.nix wants to use grub 2; override that here.
+  (lib.mkForce { boot.loader.grub.version = 1; })
+  ];
 }
