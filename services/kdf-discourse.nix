92de43dbd28701b3b51d73bb0c17dc5d2cd48e9f
Evan Danaher <github@edanaher.net>
2017-04-29 17:03:29 -0400

    kdf-discourse: Only run on host kdf.sh.  Oops.

--- services/kdf-discourse.nix
+++ services/kdf-discourse.nix
@@ -1,14 +1,16 @@
 { config, lib, pkgs, ...}:
 
 {
-  systemd.services.party-discourse = {
-    description = "Discourse docker container";
-    path = with pkgs; [ docker gawk git inetutils which ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "/var/discourse/launcher start app";
+  config = lib.mkIf (config.host.name == "kdfsh") {
+    systemd.services.party-discourse = {
+      description = "Discourse docker container";
+      path = with pkgs; [ docker gawk git inetutils which ];
+      wants = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "/var/discourse/launcher start app";
+      };
     };
   };
 }
