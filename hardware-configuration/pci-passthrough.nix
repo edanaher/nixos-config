8ca09491f34ffd92e7d78f2688bdc837e13c36d1
Evan Danaher <github@edanaher.net>
2017-11-19 13:23:15 -0500

    doyha: Initial setup.

---
+++ hardware-configuration/pci-passthrough.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+
+{
+  boot.kernelParams = [ "intel_iommu=on" ];
+
+  boot.kernelModules = [ "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];
+
+  boot.extraModprobeConfig = "options vfio-pci ids=10de:1b81,10de:10f0";
+
+ 
+  environment.systemPackages = with pkgs; [
+    virtmanager
+    qemu
+    OVMF
+  ];
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.enableKVM = true;
+
+  users.groups.libvirtd.members = [ "root" "edanaher" ];
+
+  virtualisation.libvirtd.qemuVerbatimConfig = ''
+    nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ];
+  '';
+}
--- host/hosts.nix
+++ host/hosts.nix
@@ -5,6 +5,44 @@ let
   udev-keyboard-autoplug = import ../scripts/udev-keyboard-autoplug.nix;
   acpid-script = import ../scripts/acpid-script.nix;
   hosts = {
+    "doyha" = {
+      host.class ="desktop";
+      host.boot-type = "efi";
+      #host.chileh-backups.enable = true;
+      #nix.buildCores = 4;
+      #services.exim.enable = true;
+
+      host.virtualbox.enable = false;
+
+      services.openssh.forwardX11 = true;
+      networking.firewall.allowedTCPPortRanges = [
+        { from = 6945; to = 6949; }  # bittorrent
+      ];
+
+      networking.extraHosts = ''
+        192.168.12.205 deretheni
+        169.254.94.126 gemedetw
+      '';
+
+      /*systemd.services.check-edanaher-mail = {
+        description = "Check mail for edanaher";
+        path = [ pkgs.fetchmail pkgs.procmail ];
+        serviceConfig = {
+          Type = "oneshot";
+          User = "edanaher";
+          ExecStart = "/home/edanaher/bin/bin/run_fetchmail";
+        };
+      };*/
+
+      /*systemd.timers.check-edanaher-mail = {
+        description = "Check mail for edanaher every half hour";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "*-*-* *:00,30:00";
+          Persistent = true;
+        };
+      };*/
+    };
     "chileh" = {
       host.class ="desktop";
       host.boot-type = "bios";
