316ac4fbba9b4b35e325cf9ddff91d11630d8fa6
Evan Danaher <github@edanaher.net>
2017-12-05 08:54:52 -0500

    containers: Nat through wlp9s0 for doyha.
    This should be configured per host...

--- containers.nix
+++ containers.nix
@@ -4,7 +4,7 @@
   config = lib.mkMerge [{
       networking.nat.enable = true;
       networking.nat.internalInterfaces = ["ve-+"];
-      networking.nat.externalInterface = "wlp2s0";
+      networking.nat.externalInterface = "wlp9s0";
 
       virtualisation.docker.enable = true;
       virtualisation.docker.storageDriver = "btrfs";
