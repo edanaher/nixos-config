c3b50078a978fb9672397c594cbe4d046d56ae63
Evan Danaher <github@edanaher.net>
2017-12-08 15:31:03 +0000

    angell-classes: Update to send e-mails to me on class changes.

--- services/angell-classes.nix
+++ services/angell-classes.nix
@@ -1,17 +1,18 @@
 {config, lib, pkgs, ...}:
 
-let monitor-script = pkgs.python3Packages.buildPythonApplication rec  {
+let utils = import ../utils.nix;
+    monitor-script = pkgs.python3Packages.buildPythonApplication rec  {
       name = "angell-class-monitor";
-      version = "1080f9e";
+      version = "6094532";
 
       src = pkgs.fetchFromGitHub {
         owner = "edanaher";
         repo = "angell-class-monitor";
         rev = version;
-        sha256 = "0xgafp3qzg31g34r8x2chw6wk6xvafb1kpxf3mhgd2786hc08dld";
+        sha256 = "0484nac0019vzz58wmil0q9zbrrg2zn00h8krzdhfwgi6mfr08ib";
       };
 
-      propagatedBuildInputs = [ pkgs.python3Packages.docopt ];
+      propagatedBuildInputs = with pkgs.python3Packages; [ docopt psycopg2 ];
 
       buildPhase = "";
 
@@ -19,6 +20,11 @@ let monitor-script = pkgs.python3Packages.buildPythonApplication rec  {
         mkdir -p $out/bin/
         cp generate.py $out/bin
         cp template.html $out/bin
+        substitute setup.sh $out/bin/setup.sh --replace @SUDO ${pkgs.sudo} --replace @POSTGRESQL ${pkgs.postgresql} --replace @OUT $out
+        chmod +x $out/bin/setup.sh
+
+        mkdir -p $out/etc
+        cp angell.sql $out/etc/
       '';
 
       doCheck = false;
@@ -31,15 +37,11 @@ let monitor-script = pkgs.python3Packages.buildPythonApplication rec  {
     };
 
     angell-path = "/var/www/angell-classes";
-    python = pkgs.python3.withPackages (ps: [ ps.docopt] );
     update-wrapper = pkgs.writeScriptBin "angell-classes-wrapper" ''
       #!/bin/sh
-      mkdir -p ${angell-path}/raw
-      mkdir -p /var/run/angell-classes
 
       now=`date -Iseconds`
-      cd ${monitor-script}/bin
-      ./generate.py -o ${angell-path}/new-$now.html -r ${angell-path}/raw/$now
+      ${monitor-script}/bin/generate.py -o ${angell-path}/new-$now.html -r ${angell-path}/raw/$now -m localhost
       ln -sf ${angell-path}/new-$now.html ${angell-path}/index.html
     '';
 in
@@ -57,17 +59,24 @@ in
     systemd.services.update-angell-classes = {
       description = "Scrape updates for Angell classes";
       path = with pkgs; [ curl ];
-      wants = [ "network-online.target" ];
+      wants = [ "network-online.target" "postgresql.service" ];
+      environment = { TZ = "America/Los_Angeles"; };
       serviceConfig = {
-        User = "root";
-        Group = "root";
+        User = "angell";
+        PermissionsStartOnly = "true";
         ExecStart = "${update-wrapper}/bin/angell-classes-wrapper";
         Restart = "on-failure";
         RestartSec = "4h";
       };
+
+      preStart = ''
+        ${monitor-script}/bin/setup.sh ${angell-path}
+      '';
     };
 
     systemd.timers.update-angell-classes = utils.simple-timer "daily" "Scrape updates for Angell classes daily";
+
+    users.users.angell.description = "User to run the angell-class-monitor script";
   };
   options = {
     host.angell-classes.enable = lib.mkOption {
