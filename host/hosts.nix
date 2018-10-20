61b86f1729f527244993d48f3c193dd6e2f753e6
Evan Danaher <github@edanaher.net>
2018-10-20 17:17:53 -0400

    Move kroen config to symlinked configuration/

--- host/hosts.nix
+++ host/hosts.nix
@@ -48,40 +48,6 @@ let
       };
     };
     "kroen" = {
-      nix.buildCores = 4;
-      networking.hostId = "65c89bd7";
-      host.class ="laptop";
-      services.exim.enable = true;
-
-      services.xserver.synaptics.enable = true;
-
-      services.xserver.synaptics.accelFactor = "0.05";
-      services.xserver.synaptics.minSpeed = "1";
-      services.xserver.synaptics.maxSpeed = "2.5";
-      services.xserver.synaptics.twoFingerScroll = true;
-      services.xserver.synaptics.vertEdgeScroll = true;
-      services.xserver.synaptics.additionalOptions = ''
-        Option "VertScrollDelta" "16"
-        Option "RTCornerButton" "3"
-        Option "TapButton2" "3"
-        Option "TapButton3" "2"
-        Option "ClickFinger2" "3"
-        Option "ClickFinger3" "2"
-        Option "CircularScrolling" "1"
-        Option "CircScrollTrigger" "2"
-        Option "RightButtonAreaLeft" "560"
-        Option "RightButtonAreaTop" "400"
-        Option "MiddleButtonAreaLeft" "460"
-        Option "MiddleButtonAreaRight" "559"
-        Option "MiddleButtonAreaTop" "400"
-      '';
-
-      services.udev.packages = [ udev-keyboard-autoplug ];
-      services.udev.extraRules = ''
-        ACTION=="add", ATTRS{idVendor}=="06cb", ATTRS{idProduct}=="2819", RUN+="${udev-keyboard-autoplug}/bin/udev-keyboard-autoplug.sh"
-      '';
-
-      host.touchscreen = true;
     };
     "gemedet" = {
       nix.buildCores = 8;
@@ -92,40 +58,7 @@ let
 
       boot.blacklistedKernelModules = [ "radeon" ];
     };
-    "kdfsh" = {
-      host.class ="server";
-      host.boot-type = "xen";
-      host.kdf-services.enable = true;
-      host.dovecot.enable = true;
-      host.gritonputty.enable = true;
-      host.iodine.enable = true;
-      host.randomtoby.enable = true;
-      host.caltrain.enable = true;
-      host.lwt.enable = true;
-      host.angell-classes.enable = true;
-      host.nowaytopreventthis.enable = true;
-      host.ctfwsinthepark.enable = true;
-
-      networking.interfaces.eth0 = {
-        ipv6Address = "2605:2700:0:5::4713:9cf2";
-      };
-
-      services.exim.enable = true;
-
-      networking.firewall.allowedUDPPortRanges = [
-        { from = 53; to = 53; }  # DNS (kdf-dns)
-      ];
-      networking.firewall.allowedTCPPortRanges = [
-        { from = 25; to = 25; }  # SMTP (exim)
-        { from = 587; to = 587; }  # SMTP (exim)
-        { from = 80; to = 80; }  # HTTP (nginx)
-        { from = 443; to = 443; }  # HTTPS (nginx)
-        { from = 143; to = 143; }  # IMAP (dovecot)
-        { from = 993; to = 993; }  # IMAP (dovecot)
-        { from = 5269; to = 5269; }  # XMPP (prosody)
-        { from = 5222; to = 5222; }  # XMPP (prosody)
-      ];
-    };
+    "kdfsh" = {};
   };
   hostconfig = utils.select config.host.name hosts;
 in
