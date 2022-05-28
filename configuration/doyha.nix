8f1f5ec16c4ccf006731bcadc05e95e1bf22c788
Evan Danaher <github@edanaher.net>
2022-05-28 17:22:37 -0400

    doyha: install steam.

--- configuration/doyha.nix
+++ configuration/doyha.nix
@@ -121,10 +121,15 @@
     #    nativeOnly = true;
     #  };
     #};
-    #nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    #     "steam"
-    #     "steam-original"
-    #     "steam-runtime"
-    #   ];
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+         "steam"
+         "steam-original"
+         "steam-runtime"
+       ];
   };
 }
